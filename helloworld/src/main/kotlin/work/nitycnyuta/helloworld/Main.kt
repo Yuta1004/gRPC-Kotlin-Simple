4c2e80f1560a8b5ae5c6dbf5f4eab1e92e998e29
NakagamiYuta <guguru0014@gmail.com>
2021-08-23 14:22:58 +0900

    [clean] helloworldプロジェクト削除
    Gradleに慣れるために作ってただけなので

--- helloworld/src/main/kotlin/work/nitycnyuta/helloworld/Main.kt
+++
@@ -1,7 +0,0 @@
-@file:JvmName("Main")
-
-package work.nitycnyuta.helloworld
-
-fun main() {
-    println("Hello World!!")
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 rootProject.name = "grpc_kotlin_sample"
 
 // サブプロジェクトがある場合includeで指定
-include("helloworld")
 include("protos")
 include("stub")
