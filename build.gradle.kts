ee26ed5831f53e8196de3fad32252f2444398333
NakagamiYuta <guguru0014@gmail.com>
2021-08-25 13:30:58 +0900

    [update] 使用パッケージ(protobuf)のバージョン変更
    - protobuf => 0.8.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@
 //     => マルチプロジェクト構成にするときはこのようにする
 //     => Plugin.Applyが実行される際，ProjectにTaskが追加されたりする
 plugins {
-    id("com.google.protobuf") version "0.8.15" apply false
+    id("com.google.protobuf") version "0.8.17" apply false
     kotlin("jvm") version "1.5.21" apply false
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1" apply false
     "java-library"
@@ -13,7 +13,7 @@ plugins {
 // 独自プロパティの定義 => Project.extに定義する
 ext["grpcVersion"] = "1.37.0"
 ext["grpcKotlinVersion"] = "1.1.0"
-ext["protobufVersion"] = "3.15.8"
+ext["protobufVersion"] = "3.17.0"
 
 // サブプロジェクト全体に適用する設定 => subprojectsブロック内に記述
 subprojects {
